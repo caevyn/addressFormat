b42b636252298acb225a06d9fa28810e562f66a4
Matt Murphy <m.murphy@nib.com.au>
2013-06-12 13:47:41 +1000

    use querySelector and data- attribs

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 ﻿'use strict';
 var xhr = require('xhr');
 
-function addressFormat(addressId, options) {
+function addressFormat(addressId, container, options) {
+    var el = container;
+    if(el === undefined) {
+        el = document;
+    }
     if (options === undefined) {
         options = { };
     }
@@ -16,28 +20,28 @@ function addressFormat(addressId, options) {
     },
     function onSuccess(req) {
         var result = JSON.parse(req.responseText);
-
-        var address = document.getElementsByClassName(options.cssPrefix + '-address')[0];
+        
+        var address = el.querySelector("input[data-qas='address']");
         if (address !== undefined) {
             address.value = result.streetAddress;
         }
 
-        var locality = document.getElementsByClassName(options.cssPrefix + '-locality')[0];
+        var locality = el.querySelector("input[data-qas='locality']");
         if (locality !== undefined) {
             locality.value = result.locality;
         }
         
-        var country = document.getElementsByClassName(options.cssPrefix + '-country')[0];
+        var country = el.querySelector("input[data-qas='country']");
         if (country !== undefined) {
             country.value = result.country;
         }
         
-        var postcode = document.getElementsByClassName(options.cssPrefix + '-postcode')[0];
+        var postcode = el.querySelector("input[data-qas='postcode']");;
         if (postcode !== undefined) {
             postcode.value = result.postcode;
         }
         
-        var state = document.getElementsByClassName(options.cssPrefix + '-state')[0];
+        var state = el.querySelector("input[data-qas='state']");;
         if (state !== undefined) {
             state.value = result.state;
         }
