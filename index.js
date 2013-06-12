32213dc49298bc2743351605bd34a24992c49818
Matt Murphy <m.murphy@nib.com.au>
2013-06-12 15:47:48 +1000

    refactoring

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ var xhr = require('xhr');
 
 function addressFormat(addressId, container, options) {
     var el = container;
-    if(el === undefined || el === null) {
+    if (el === undefined || el === null) {
         el = document;
     }
     if (options === undefined || el === null) {
-        options = { };
+        options = {};
     }
     options.url = options.url || '../address/';
-        
+
+    function updateField(field, value) {
+        var input = el.querySelector("[data-qas='" + field + "']");
+        if (input !== null) {
+            input.value = value;
+        }
+    }
+
     xhr({
         url: options.url + addressId,
         headers: {
@@ -19,31 +26,12 @@ function addressFormat(addressId, container, options) {
     },
     function onSuccess(req) {
         var result = JSON.parse(req.responseText);
-        
-        var address = el.querySelector("[data-qas='address']");
-        if (address !== null) {
-            address.value = result.streetAddress;
-        }
 
-        var locality = el.querySelector("[data-qas='locality']");
-        if (locality !== null) {
-            locality.value = result.locality;
-        }
-        
-        var country = el.querySelector("[data-qas='country']");
-        if (country !== null) {
-            country.value = result.country;
-        }
-        
-        var postcode = el.querySelector("[data-qas='postcode']");;
-        if (postcode !== null) {
-            postcode.value = result.postcode;
-        }
-        
-        var state = el.querySelector("[data-qas='state']");;
-        if (state !== null) {
-            state.value = result.state;
-        }
+        updateField('address', result.streetAddress);
+        updateField('locality', result.locality);
+        updateField('country', result.country);
+        updateField('postcode', result.postcode);
+        updateField('state', result.state);
     },
     function onError(err) {
         console.log('There was an error getting the address details: ' + err.message);
